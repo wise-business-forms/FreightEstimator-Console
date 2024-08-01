8804faf9d410e992c9df309008268b75c82c4645
Paul Menefee <pmenefee@wbf.com>
2024-08-01 09:06:57 -0400

    Add nunit

---
+++ FreightEstimator-Console-NUnit/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace FreightEstimator_Console_NUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
